860d3e88b18375beca694f03b60802a8fccc89aa
Gabriel Lira <gabriel.tertulino@proton.me>
2024-06-07 14:06:15 -0300

    commit 16.0

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,41 @@
 const radioInputs = document.querySelectorAll('input[type=radio]');
-console.log(radioInputs);
 const metricContainer = document.querySelector('.form__text-inputs.metric');
-console.log(metricContainer);
 const imperialContainer = document.querySelector('.form__text-inputs.imperial');
-console.log(imperialContainer);
+const textInputs = document.querySelectorAll('input[type=text]');
+let heightValue = null;
+let weightValue = null;
 
+
+
+
+/* Iterates over radio type inputs, adds a change event listener and triggers the showOrHide function */
 radioInputs.forEach(radio => {
-  radio.addEventListener('change', function() {
-    if (this.id === "metric" && this.checked) {
-      metricContainer.style.display = "flex";
-      imperialContainer.style.display = "none";
-    } else if (this.id === "imperial" && this.checked) {
-      imperialContainer.style.display = "block";
-      metricContainer.style.display = "none";
+  radio.addEventListener('change', showOrHide);
+});
+
+/* This function shows or hides metric or imperial type containers depending on the selected input radio.*/
+function showOrHide() {
+  if (this.id === "metric" && this.checked) {
+    metricContainer.style.display = "flex";
+    metricContainer.style.flexWrap = "wrap";
+    imperialContainer.style.display = "none";
+  } else if (this.id === "imperial" && this.checked) {
+    imperialContainer.style.display = "block";
+    metricContainer.style.display = "none";
+  }
+}
+
+
+textInputs.forEach(input => {
+  input.addEventListener('input', function(){
+    if(input.id === "height-cm") {
+      heightValue = parseFloat(input.value) / 100;
+    } else if (input.id === "weight-kg") {
+      weightValue = parseFloat(input.value);
+    }
+
+    if (heightValue !== null && weightValue !== null) {
+      const bmiResult = weightValue / (heightValue * heightValue);
     }
   })
 })
